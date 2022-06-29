70137a51b483387dbb723b9c019c1d81931a68b9
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-06-29 06:54:43 +0300

    Update shop.py

--- first_steps_in_OOP - Exercise/shop.py
+++ first_steps_in_OOP - Exercise/shop.py
@@ -1,3 +1,6 @@
+# Create a class called Shop. Upon initialization, it should receive a name (string) and items (list). Create a method called get_items_count() 
+# which should return the number of items in the store.
+
 class Shop:
     def __init__(self, name, items):
         self. name = name
