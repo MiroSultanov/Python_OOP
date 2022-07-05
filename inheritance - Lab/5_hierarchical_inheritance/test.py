adfe81c9f767b15e76f5ee576ba00379599a6ca1
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-05 06:46:08 +0300

    Update test.py

--- inheritance - Lab/5_hierarchical_inheritance/test.py
+++ inheritance - Lab/5_hierarchical_inheritance/test.py
@@ -1,3 +1,11 @@
+# In a folder called project create three files: animal.py, dog.py, and cat.py.
+# In each file, create its corresponding class - Animal, Dog, and Cat:
+# •	Animal with a single method eat() that returns: "eating..."
+# •	Dog with a single method bark() that returns: "barking..."
+# •	Cat with a single method meow() that returns: "meowing..."
+# Both Dog and Cat should inherit from Animal.
+
+
 from project.cat import Cat
 from project.dog import Dog
 
