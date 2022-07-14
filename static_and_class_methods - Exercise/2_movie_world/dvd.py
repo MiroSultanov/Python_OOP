184ea540828a3ee0a85eac5c773c6ed69a26f762
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-15 00:07:15 +0300

    Add files via upload

---
+++ static_and_class_methods - Exercise/2_movie_world/dvd.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+class DVD:
+
+    def __init__(self, name: str, id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, id_: int, name: str, date: str, age_restriction: int):
+        dvd_date = datetime.strptime(date, '%d.%m.%Y')
+        return cls(name, id_, int(dvd_date.strftime("%Y")), dvd_date.strftime("%B"), age_restriction)
+
+    def __repr__(self):
+        status = 'rented' if self.is_rented else 'not rented'
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction {self.age_restriction}. Status: {status}"
