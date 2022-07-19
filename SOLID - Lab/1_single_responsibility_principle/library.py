3613a7af0f4461189c1be4eb3f55a8a5b6f36dc8
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-19 06:27:40 +0300

    Update library.py

--- SOLID - Lab/1_single_responsibility_principle/library.py
+++ SOLID - Lab/1_single_responsibility_principle/library.py
@@ -1,3 +1,7 @@
+# Refactor the provided code, so there is a separate class called Library, which contains books and has a method called find_book(title) that returns the book with
+# the given title. Remove the unnecessary code from the Book class.
+
+
 from single_responsibility_principle.book import Book
 
 class Library:
