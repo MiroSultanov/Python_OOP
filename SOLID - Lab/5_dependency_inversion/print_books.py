ca1bbaae500c0139abd09245e8423389266577bf
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-19 06:30:12 +0300

    Update print_books.py

--- SOLID - Lab/5_dependency_inversion/print_books.py
+++ SOLID - Lab/5_dependency_inversion/print_books.py
@@ -1,3 +1,7 @@
+# We want to print books, but before printing the book, we should format it. To accomplish this, we have a class Printer that can print books and a class 
+# Formatter which is used by the Printer. Refactor the provided code that breaks the DIP because both Printer and Formatter depend on concretions, not abstractions, 
+# by creating abstractions and injecting them wherever needed.
+
 from abc import ABC, abstractmethod
 
 
