72d58ef63210719c9887ada60df3b2b104e36cc2
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-23 10:43:50 +0300

    Update custom_range.py

--- iterators_and_generators - Lab/custom_range.py
+++ iterators_and_generators - Lab/custom_range.py
@@ -1,3 +1,6 @@
+# Create a class called custom_range that receives a start (int) and an end (int) upon initialization. Implement the __iter__ and __next__ methods, 
+# so the iterator returns the numbers from the start to the end (inclusive).
+
 class custom_range:
 
     def __init__(self, start, end):
@@ -19,3 +22,9 @@ if __name__ == '__main__':
     one_to_ten = custom_range(1, 10)
     for num in one_to_ten:
         print(num)
+        
+        
+# Test Code
+# one_to_ten = custom_range(1, 10)
+# for num in one_to_ten:
+#     print(num)
