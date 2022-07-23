2a6a4d3a6861067163c6957c800894d744a04dfb
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-23 10:44:33 +0300

    Update reverse_iter.py

--- iterators_and_generators - Lab/reverse_iter.py
+++ iterators_and_generators - Lab/reverse_iter.py
@@ -1,3 +1,6 @@
+# Create a class called reverse_iter which should receive an iterable upon initialization. Implement the __iter__ and __next__ methods, so the iterator returns the 
+# items of the iterable in reversed order.
+
 class reverse_iter:
 
     def __init__(self, iterable):
@@ -18,3 +21,8 @@ if __name__ == '__main__':
     reversed_list = reverse_iter([1, 2, 3, 4])
     for item in reversed_list:
         print(item)
+        
+#  Test Code
+# reversed_list = reverse_iter([1, 2, 3, 4])
+# for item in reversed_list:
+#     print(item)
