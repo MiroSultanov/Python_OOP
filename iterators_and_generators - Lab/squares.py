b35e785d66d5b6e22769252d9c79691673cb47a6
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-23 10:45:46 +0300

    Update squares.py

--- iterators_and_generators - Lab/squares.py
+++ iterators_and_generators - Lab/squares.py
@@ -1,6 +1,12 @@
+# Create a generator function called squares that should receive a number n. It should generate the squares of all numbers from 1 to n (inclusive).
+
 def squares(n: int) -> iter:
     for i in range(1, n + 1):
         yield i**2
 
 
 print(list(squares(5)))
+
+
+# Test Code
+# print(list(squares(5)))
