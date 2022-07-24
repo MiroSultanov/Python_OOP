65185b783dd1f992fc95cc1df8c63c83e4e139a8
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-24 22:57:21 +0300

    Update sequence_repeat.py

--- iterators_and_generators - Exercise/sequence_repeat.py
+++ iterators_and_generators - Exercise/sequence_repeat.py
@@ -1,3 +1,7 @@
+# Create a class called sequence_repeat which should receive a sequence and a number upon initialization. Implement an iterator to return the given elements, 
+# so they form a string with a length - the given number. If the number is greater than the number of elements, then the sequence repeats as necessary. 
+# For more clarification, see the examples:
+
 class sequence_repeat:
 
     def __init__(self, sequence: str, number: int):
@@ -22,3 +26,12 @@ if __name__ == '__main__':
     result = sequence_repeat('abc', 5)
     for item in result:
         print(item, end='')
+        
+#  Test Code
+# result = sequence_repeat('abc', 5)
+# for item in result:
+#     print(item, end ='')
+    
+# result = sequence_repeat('I Love Python', 3)
+# for item in result:
+#     print(item, end ='')
