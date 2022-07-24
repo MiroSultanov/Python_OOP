812d4dec83cc7ec8a7e1de18b951163d48d692e9
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-24 22:59:28 +0300

    Update take_skip.py

--- iterators_and_generators - Exercise/take_skip.py
+++ iterators_and_generators - Exercise/take_skip.py
@@ -1,3 +1,6 @@
+# Create a class called take_skip. Upon initialization, it should receive a step (int) and a count (int). Implement the __iter__ and __next__ functions. 
+# The iterator should return the count numbers (starting from 0) with the given step. For more clarification, see the examples:
+
 class take_skip:
 
     def __init__(self, step: int, count: int):
@@ -24,3 +27,13 @@ if __name__ == '__main__':
     numbers = take_skip(10, 5)
     for number in numbers:
         print(number)
+        
+        
+#  Test Code
+# numbers = take_skip(2, 6)
+# for number in numbers:
+#     print(number)
+    
+# numbers = take_skip(10, 5)
+# for number in numbers:
+#     print(number)
