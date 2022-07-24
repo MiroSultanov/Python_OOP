b8162b827ccc1c320003c1f181706142ae232036
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-24 22:54:11 +0300

    Update fibonacci_generator.py

--- iterators_and_generators - Exercise/fibonacci_generator.py
+++ iterators_and_generators - Exercise/fibonacci_generator.py
@@ -1,3 +1,6 @@
+# Create a generator function called fibonacci() that generates the Fibonacci numbers infinitely. The first two numbers in the sequence are always 0 and 1. 
+# Each following Fibonacci number is created by the sum of the current number with the previous one.
+
 def fibonacci():
     prior = 0
     current = 1
@@ -12,3 +15,14 @@ def fibonacci():
 generator = fibonacci()
 for i in range(10):
     print(next(generator))
+    
+    
+#  Test Code
+# generator = fibonacci()
+# for i in range(5):
+#     print(next(generator))
+    
+    
+# generator = fibonacci()
+# for i in range(1):
+#     print(next(generator))
