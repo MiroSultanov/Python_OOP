15e176526ea21c2567cf0ebd7528f5865e58ce67
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-24 22:56:29 +0300

    Update reader.py

--- iterators_and_generators - Exercise/reader.py
+++ iterators_and_generators - Exercise/reader.py
@@ -1,3 +1,6 @@
+# Create a generator function called read_next() which should receive a different number of arguments (all iterable). On each iteration, the function should 
+# return each element from each sequence.
+
 def read_next(*args):
     for arg in args:
         for el in arg:
@@ -5,3 +8,10 @@ def read_next(*args):
 
 for item in read_next('string', (2,), {'d': 1, 'i': 2, 'c': 3, 't': 4}):
     print(item, end='')
+    
+#  Test Code
+# for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
+#     print(item, end='')
+    
+# for i in read_next("Need", (2, 3), ["words", "."]):
+#     print(i)
