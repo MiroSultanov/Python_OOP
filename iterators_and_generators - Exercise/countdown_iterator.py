fce36a2ee8aed98e62c45a2061ede1e9c1fe275c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-24 22:52:31 +0300

    Update countdown_iterator.py

--- iterators_and_generators - Exercise/countdown_iterator.py
+++ iterators_and_generators - Exercise/countdown_iterator.py
@@ -1,3 +1,6 @@
+# Create a class called countdown_iterator. Upon initialization, it should receive a count. Implement the iterator to return each countdown number 
+# (from count to 0 inclusive), separated by a single space.
+
 class countdown_iterator:
 
     def __init__(self, count: int):
@@ -17,3 +20,14 @@ if __name__ == '__main__':
     iterator = countdown_iterator(10)
     for item in iterator:
         print(item, end=" ")
+        
+        
+# Test Code
+# iterator = countdown_iterator(10)
+# for item in iterator:
+#     print(item, end=" ")
+    
+    
+# iterator = countdown_iterator(0)
+# for item in iterator:
+#     print(item, end=" ")
