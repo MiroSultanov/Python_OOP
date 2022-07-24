8f16fc396316df9e93681c2a4af794c7bdc63b71
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-24 22:53:23 +0300

    Update dictionary_iterator.py

--- iterators_and_generators - Exercise/dictionary_iterator.py
+++ iterators_and_generators - Exercise/dictionary_iterator.py
@@ -1,3 +1,6 @@
+# Create a class called dictionary_iter. Upon initialization, it should receive a dictionary object. Implement the iterator to return each key-value pair of
+# the dictionary as a tuple of two elements (the key and the value).
+
 class dictionary_iter:
 
     def __init__(self, dict_obj: dict):
@@ -18,3 +21,14 @@ if __name__ == '__main__':
     result = dictionary_iter({1: "1", 2: "2"})
     for x in result:
         print(x)
+        
+        
+# Test Code
+# result = dictionary_iter({1: "1", 2: "2"})
+# for x in result:
+#     print(x)
+    
+    
+# result = dictionary_iter({"name": "Peter", "age": 24})
+# for x in result:
+#     print(x)
