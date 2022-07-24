046c1dd4f1a275ed0a3e8a534ebd1bb511a22a0c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-24 22:55:48 +0300

    Update prime_numbers.py

--- iterators_and_generators - Exercise/prime_numbers.py
+++ iterators_and_generators - Exercise/prime_numbers.py
@@ -1,3 +1,5 @@
+# Create a generator function called get_primes() which should receive a list of integer numbers and return a list containing only the prime numbers from the initial list
+
 def get_primes(num_list: list):
     def is_prime(num: int):
         if num > 1:
@@ -13,3 +15,9 @@ def get_primes(num_list: list):
 
 
 print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
+
+
+# Test Code
+# print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
+
+# print(list(get_primes([-2, 0, 0, 1, 1, 0])))
