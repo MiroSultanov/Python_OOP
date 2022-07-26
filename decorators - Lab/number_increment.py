ce4d23d08656d01e377ae4cbba92ac2b2b07f211
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-26 06:42:23 +0300

    Update number_increment.py

--- decorators - Lab/number_increment.py
+++ decorators - Lab/number_increment.py
@@ -1,3 +1,14 @@
+# Having the following code:
+#     def number_increment(numbers):
+
+#     def increase():
+
+#         # TODO: Implement
+
+#     return increase()
+
+# Complete the code, so it works as expected.
+
 def number_increment(numbers):
 
     def increase():
