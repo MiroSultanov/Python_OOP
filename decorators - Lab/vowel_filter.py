e52ebd689c943ec01d32922e5a9f6a9b3c1cef44
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-26 06:39:35 +0300

    Add files via upload

---
+++ decorators - Lab/vowel_filter.py
@@ -0,0 +1,14 @@
+def vowel_filter(function):
+    vowels = set('aeiou' + 'aeiou'.upper())
+
+    def wrapper():
+        result = function()
+        return [c for c in result if c in vowels]
+    return wrapper
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
+
