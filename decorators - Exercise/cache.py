8bf08523a56b11ba4d0be4c293106a24cc93d4e8
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-27 06:56:39 +0300

    Update cache.py

--- decorators - Exercise/cache.py
+++ decorators - Exercise/cache.py
@@ -1,3 +1,24 @@
+# Create a decorator called cache. It should store all the returned values of the recursive function fibonacci. You are provided with this code:
+#     def cache(func):
+
+#     # TODO: Implement
+    
+# @cache
+
+# def fibonacci(n):
+
+# if n < 2:
+
+#     return n
+
+# else:
+
+#     return fibonacci(n-1) + fibonacci(n-2)
+
+# You need to create a dictionary called log that will store all the n's (keys) and the returned results (values) and attach that dictionary to the fibonacci 
+# function as a variable called log, so when you call it, it returns that dictionary.
+
+
 def cache(func_ref):
     memo = {}
 
@@ -19,7 +40,7 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-
+# Test Code
 
 fibonacci(3)
 print(fibonacci.log)
