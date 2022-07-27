01420253ce96df9da6760391fc1cfeafd43710f8
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-27 07:01:02 +0300

    Update store_results.py

--- decorators - Exercise/store_results.py
+++ decorators - Exercise/store_results.py
@@ -1,9 +1,14 @@
+# Create a class called store_results. It should be used as a decorator and store information about the executed functions in a file called results.txt in the 
+# format: "Function {func_name} was add called. Result: {func_result}"
+
 def store_results(func_ref):
     def wrapper(*args):
         result = func_ref(*args)
         return f"Function {func_ref.__name__} was called. Result: {result}"
 
     return wrapper
+
+# Test Code
 
 @store_results
 def add(a, b):
