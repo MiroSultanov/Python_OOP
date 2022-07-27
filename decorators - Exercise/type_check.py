93c05b865b7eef938882f61171a63e608168045b
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-27 07:02:02 +0300

    Update type_check.py

--- decorators - Exercise/type_check.py
+++ decorators - Exercise/type_check.py
@@ -1,3 +1,6 @@
+# Create a decorator called type_check. It should receive a type (int/float/str/…), and it should check if the parameter passed to the decorated function is of 
+# the type given to the decorator. If it is, execute the function and return the result, otherwise return "Bad Type".
+
 def type_check(type):
     def decorator(func_ref):
         def wrapper(argument):
@@ -8,6 +11,8 @@ def type_check(type):
         return wrapper
 
     return decorator
+
+# Test Code
 
 @type_check(int)
 def times2(num):
