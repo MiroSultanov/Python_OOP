6b19764e53a8937326706e636c76b1df6afac93e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-27 07:00:15 +0300

    Update logged.py

--- decorators - Exercise/logged.py
+++ decorators - Exercise/logged.py
@@ -1,9 +1,14 @@
+# Create a decorator called logged. It should return the name of the function that is being called and its parameters. It should also return the result of the execution of 
+# the function being called
+
 def logged(func_ref):
     def wrapper(*args):
         result = func_ref(*args)
         return f"you called {func_ref.__name__}{args}\nit returned {result}"
 
     return wrapper
+
+# Test Code
 
 @logged
 def func(*args):
